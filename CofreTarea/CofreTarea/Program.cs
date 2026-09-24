549f7b5a3540f781037ae3787102852a7991f391
Nathan <n4th4n.ed@gmail.com>
2026-09-23 23:20:07 -0300

    Solucion

---
+++ CofreTarea/CofreTarea/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CofreTarea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CofreMadera cofreMadera = new CofreMadera();
+            cofreMadera.Abrir();
+            Console.ReadKey();
+
+        }
+
+       internal class CofreMadera
+        {
+            public int Oro = 75;
+            public bool Abierto = false;
+
+            public void Abrir()
+            {
+                if (Abierto == false)
+                {
+                    Console.WriteLine("Oro dentro del cofre: " + Oro);
+                }
+            }
+        }
+    }
+}
